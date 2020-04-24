d5975e7b0f8c8b252b3c43ba1790bb4e32e2dfb6
Willian da Rosa <will.rosa.gvt@gmail.com>
2020-04-23 21:00:06 -0300

    criando gênero

---
+++ app/Http/Controllers/Api/GenreController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Genre;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Validation\ValidationException;
+
+class GenreController extends Controller
+{
+
+    private $rules = [
+        'name' => 'required|max:255'
+    ];
+
+    /**
+     * @return Genre[]
+     */
+    public function index()
+    {
+        return Genre::all();
+    }
+
+    /**
+     * @param Request $request
+     * @return mixed
+     * @throws ValidationException
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request, $this->rules);
+        return Genre::create($request->all());
+    }
+
+    /**
+     * @param Genre $genre
+     * @return Genre
+     */
+    public function show(Genre $genre)
+    {
+        return $genre;
+    }
+
+
+    /**
+     * @param Request $request
+     * @param Genre $genre
+     * @return Genre
+     * @throws ValidationException
+     */
+    public function update(Request $request, Genre $genre)
+    {
+        $this->validate($request, $this->rules);
+        $genre->update($request->all());
+        return $genre;
+    }
+
+    /**
+     * @param Genre $genre
+     * @return Response
+     * @throws \Exception
+     */
+    public function destroy(Genre $genre)
+    {
+        $genre->delete();
+        return response()->noContent();
+    }
+}
