035172643b5ac5724ee9da6c23a3b98c1b6a1692
Willian da Rosa <will.rosa.gvt@gmail.com>
2023-01-18 23:07:20 -0300

    Atribuindo mudanças do estado do filtro ao filter manager - Part 1

--- frontend/src/hooks/useFilter.ts
+++ frontend/src/hooks/useFilter.ts
@@ -1,14 +1,46 @@
-import {useReducer, useState} from "react";
+import {Dispatch, Reducer, useReducer, useState} from "react";
 import reducer, {INITIAL_STATE} from "../store/filter";
+import {Actions as FilterActions, State as FilterState} from "../store/filter/types";
+import {MUIDataTableColumn} from "mui-datatables";
 
-export default function useFilter() {
-    const [filterState, dispatch] = useReducer(reducer, INITIAL_STATE);
+interface FilterManagerOtptions {
+    columns: MUIDataTableColumn[];
+    rowsPerPage: number;
+    rowsPerPageOptions: number[];
+    debounceTime: number;
+}
+
+export default function useFilter(options: FilterManagerOtptions) {
+    const filterManager = new FilterManager(options);
+
+    const [filterState, dispatch] = useReducer<Reducer<FilterState, FilterActions>>(reducer, INITIAL_STATE);
     const [totalRecords, setTotalRecords] = useState<number>(0)
 
+    filterManager.state = filterState;
+    filterManager.dispatch = dispatch;
+
     return {
+        filterManager,
         filterState,
         dispatch,
         totalRecords,
         setTotalRecords
     }
+}
+
+export class FilterManager {
+    state: FilterState = null as any;
+    dispatch: Dispatch<FilterActions> = null as any;
+    columns: MUIDataTableColumn[];
+    rowsPerPage: number;
+    rowsPerPageOptions: number[];
+    debounceTime: number;
+
+    constructor(options: FilterManagerOtptions) {
+        this.columns = options.columns;
+        this.rowsPerPage = options.rowsPerPage;
+        this.rowsPerPageOptions = options.rowsPerPageOptions;
+        this.debounceTime = options.debounceTime;
+    }
+
 }
